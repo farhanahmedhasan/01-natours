415fc9e8de358e89bdfe081c23bb977e080799af
Hasan <aaxhasandjmamunxyz2015@gmail.com>
2021-07-11 18:27:30 +0600

    Turned off timer for slider

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer } from 'react';
+import React, { useContext, useReducer } from 'react';
 import { reducer, initialState } from './redeucer';
 
 const AppContext = React.createContext();
